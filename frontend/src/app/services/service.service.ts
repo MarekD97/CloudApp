61c151717ad5d13549cf85d600cf4d22f6e095c0
MarekD97 <mdorosz2@wp.pl>
2021-03-11 19:40:02 +0100

    Add components

--- frontend/src/app/services/service.service.ts
+++ frontend/src/app/services/service.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+const baseURL = 'http://localhost:8080/api/players';
 @Injectable({
   providedIn: 'root'
 })
-export class ServiceService {
+export class ClientService {
 
-  constructor() { }
+  headElements = [
+    "player_id",
+    "name",
+    "nationality",
+    "position",
+    "overall",
+    "age",
+    "hits",
+    "potential",
+    "team"
+  ]
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any> {
+    return this.http.get(baseURL);
+  }
 }
