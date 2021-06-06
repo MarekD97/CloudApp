7cd5cc000dd808d55e38ed5b640524b013956f1e
MarekD97 <mdorosz2@wp.pl>
2021-06-06 17:30:37 +0200

    Add persistent volume for database

--- kubernetes/kubernetes.sh
+++ kubernetes/kubernetes.sh
@@ -1,10 +1,16 @@
-
+# Create cluster
 kind create cluster --config=kind-cluster.yml
 
+# Load docker images
 kind load docker-image cloudapp_frontend
 kind load docker-image cloudapp_backend
 kind load docker-image cloudapp_db
 
+# Apply PV & PVC
+kubectl apply -f db-persistent-volume.yml
+kubectl apply -f db-persistent-volume-claim.yml
+
+# Apply pods and services
 kubectl apply -f database.yml
 kubectl apply -f backend.yml
 kubectl apply -f frontend.yml
